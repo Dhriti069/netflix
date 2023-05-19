03e1f5199e0b6ac9517a48999db4e290befacda9
Dhriti069 <98838127+Dhriti069@users.noreply.github.com>
2023-05-19 22:03:39 +0530

    amar

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from "../../logo.png"
+
+const Header = () => {
+   
+  return (
+    <nav className="Header">
+
+        <img src={logo} alt="logo" />
+    </nav>
+  )
+}
+
+export default Header
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        ghjhmb,zmdbnskjnkjn b
+      </p>
+    </div>
+  )
+}
+
+export default Home
